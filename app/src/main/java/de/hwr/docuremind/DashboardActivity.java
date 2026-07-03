fa2d606f3611f39fbe218ccf409cb61c8f66e041
darianomorowa <darianomorowa@gmail.com>
2026-07-03 16:01:38 +0200

    Add dashboard navigation logic

--- app/src/main/java/de/hwr/docuremind/DashboardActivity.java
+++ app/src/main/java/de/hwr/docuremind/DashboardActivity.java
@@ -1,24 +1,43 @@
 package de.hwr.docuremind;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Button buttonAddDocument;
+    private Button buttonSettings;
+    private TextView textExampleDocument;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buttonAddDocument = findViewById(R.id.buttonAddDocument);
+        buttonSettings = findViewById(R.id.buttonSettings);
+        textExampleDocument = findViewById(R.id.textExampleDocument);
+
+        buttonAddDocument.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardActivity.this, AddDocumentActivity.class);
+            startActivity(intent);
+        });
+
+        buttonSettings.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+
+        textExampleDocument.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardActivity.this, DetailActivity.class);
+            intent.putExtra("documentName", "Reisepass");
+            intent.putExtra("documentCategory", "Ausweis");
+            intent.putExtra("documentDate", "31.12.2026");
+            startActivity(intent);
         });
     }
 }

f198e244b5ee8a9a44294dde240469205b278aa2
darianomorowa <darianomorowa@gmail.com>
2026-07-03 16:18:31 +0200

    Add document form button logic

--- app/src/main/java/de/hwr/docuremind/AddDocumentActivity.java
+++ app/src/main/java/de/hwr/docuremind/AddDocumentActivity.java
@@ -1,24 +1,30 @@
 package de.hwr.docuremind;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AddDocumentActivity extends AppCompatActivity {
+
+    private Button buttonSaveDocument;
+    private Button buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_document);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buttonSaveDocument = findViewById(R.id.buttonSaveDocument);
+        buttonBack = findViewById(R.id.buttonBack);
+
+        buttonSaveDocument.setOnClickListener(view -> {
+            Toast.makeText(AddDocumentActivity.this, "Speichern folgt mit Firebase", Toast.LENGTH_SHORT).show();
+        });
+
+        buttonBack.setOnClickListener(view -> {
+            finish();
         });
     }
 }
